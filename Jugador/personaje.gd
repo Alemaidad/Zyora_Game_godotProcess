789757ed164b14548f1050ac3dff2aec65de4b3c
Alemaidad <maisdel2323@gmail.com>
2025-10-20 12:17:06 -0500

    animation progress

--- Jugador/personaje.gd
+++ Jugador/personaje.gd
@@ -1,7 +1,37 @@
 class_name Personaje
 extends CharacterBody2D
+@onready var Sprite = $Animaciones
 @export var speed = 100
-@onready var Animaciones =$Animaciones
+
+func _process(delta: float) -> void:
+	
+	var move = false
+	
+	
+	
+	if Input.is_action_pressed("Left"):
+		Sprite.play("walk_left")
+		move = true
+	elif Input.is_action_pressed("rigth"):
+		Sprite.play("walk_rigth")
+		move=true
+	elif Input.is_action_pressed("Up"):
+		Sprite.play("walt_back")
+		move=true
+	elif Input.is_action_pressed("abajo"):
+		Sprite.play("walk_front")
+		move = true
+		
+	if Input.is_action_just_pressed("abajo"):
+		Sprite.play("static_front")
+		move=false
+		
+	
+	
+	if not move:
+		if Input.is_action_just_pressed("Up"):
+			Sprite.play("run_back")
+		
 
 func _physics_process(delta: float) -> void:
 	
@@ -12,7 +42,10 @@ func _physics_process(delta: float) -> void:
 	var vertical = Input.get_axis("Up","abajo")
 	velocity.y = speed * vertical
 	
-	#hello
+	#animations
+
+
+	
 	
 	
 	
