207215aeafeec4e08cceb903e4ede5fada7edd3d
Katy <131321043+katiusca4567@users.noreply.github.com>
2025-10-22 02:02:17 -0500

    finished move animation
    run is
    WASD + J
    
    a + d = null
    w + s = null

--- Jugador/personaje.gd
+++ Jugador/personaje.gd
@@ -3,53 +3,70 @@ extends CharacterBody2D
 @onready var Sprite = $Animaciones
 @export var speed = 100
 
-func _process(delta: float) -> void:
+func _process(delta):
 	
 	var move = false
 	
 	
-	
-	if Input.is_action_pressed("Left"):
+	#Animacion de caminar
+	if Input.is_action_pressed("Left") and Input.is_action_pressed("primary"):
+		Sprite.play("run_left")
+		move= true
+	elif Input.is_action_pressed("Left"):
 		Sprite.play("walk_left")
 		move = true
+	elif Input.is_action_pressed("rigth") and Input.is_action_pressed("primary"):
+		Sprite.play("run_rigth")
+		move=true
 	elif Input.is_action_pressed("rigth"):
 		Sprite.play("walk_rigth")
+		move = true	
+	elif Input.is_action_pressed("Up") and Input.is_action_pressed("primary"):
+		Sprite.play("run_back")
 		move=true
 	elif Input.is_action_pressed("Up"):
-		Sprite.play("walt_back")
-		move=true
+		Sprite.play("walk_back")
+		move = true
+	elif Input.is_action_pressed("abajo") and Input.is_action_pressed("primary"):
+		Sprite.play("run_front")
+		move = true
 	elif Input.is_action_pressed("abajo"):
 		Sprite.play("walk_front")
 		move = true
-		
-	if Input.is_action_just_pressed("abajo"):
-		Sprite.play("static_front")
-		move=false
-		
-	
-	
+		#animacion de personaje quieto
 	if not move:
-		if Input.is_action_just_pressed("Up"):
-			Sprite.play("run_back")
-		
+		match Sprite.animation:
+			"walk_left":
+				Sprite.play("static_left")
+			"walk_rigth":
+				Sprite.play("static_rigth")
+			"walk_front":
+				Sprite.play("static_front")
+			"walk_back":
+				Sprite.play("static_back")
+			"run_left":
+				Sprite.play("static_left")
+			"run_rigth":
+				Sprite.play("static_rigth")
+			"run_back":
+				Sprite.play("static_back")
+			"run_front":
+				Sprite.play("static_front")
 
-func _physics_process(delta: float) -> void:
+func _physics_process(delta):
 	
 	#Move Horizontal
 	var Horizontal = Input.get_axis("Left","rigth")
-	velocity.x = speed * Horizontal
-	
 	var vertical = Input.get_axis("Up","abajo")
 	velocity.y = speed * vertical
+	velocity.x = speed * Horizontal
 	
-	#animations
-
-
-	
-	
-	
-	
-	
+	#Sprint
+	if Input.is_action_pressed("primary"): 
+		if Horizontal !=0:
+			velocity.x = (speed * 2) * Horizontal
+		if  vertical !=0:
+			velocity.y = (speed * 2 ) * vertical
 	move_and_slide()
 	
 	
